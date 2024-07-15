33b21f81146f38551d43d2619011a427d0ab8d32
Matheus Eufrasio <you@example.com>
2024-07-15 19:47:32 -0300

    feat: add home route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 |
 */
 
+Route::any('/home', [HomeController::class, 'home']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
