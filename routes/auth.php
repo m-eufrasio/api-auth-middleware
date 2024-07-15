6bef6fe0c8dbfb56312d8a4c67b650e6900d8e3e
Matheus Eufrasio <you@example.com>
2024-07-15 19:47:10 -0300

    feat: add home route to redirect

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,12 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::any('/home', [HomeController::class, 'home'])
+                ->middleware('guest')
+                ->name('home');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest')
