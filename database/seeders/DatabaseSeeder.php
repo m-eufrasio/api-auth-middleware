2992c3ddf4886d716e84517696d5a51440b6dcd3
Matheus Eufrasio <you@example.com>
2024-07-19 07:55:35 -0300

    feat: add Post seeder to call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UsersSeeder::class,
+            // UsersSeeder::class,
+            PostsSeeder::class,
         ]);
     }
 }
