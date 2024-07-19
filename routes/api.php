e3d72a122ea1b949336080ff58c709724a6ac18c
Matheus Eufrasio <you@example.com>
2024-07-19 07:56:13 -0300

    feat(route): add Post route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,15 @@ Route::any('/home', [HomeController::class, 'home']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    // Route::get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+
+    // Route::controller(PostController::class)->group(function () {
+    //     Route::get('/index', 'index');
+    // });
+
+    Route::prefix('post')->group(function () {
+        Route::get('/index/{id}', [PostController::class, 'index']);
     });
 });
