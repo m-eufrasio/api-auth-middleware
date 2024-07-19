8d8f94d70b2942f973533d47bb3a2017b06304cf
Matheus Eufrasio <you@example.com>
2024-07-19 07:54:33 -0300

    feat: add Gate to Auth provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,8 @@ class AuthServiceProvider extends ServiceProvider
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        //
+        Gate::define('index-post', function (User $user) {
+            return $user->hasPermission('index-post');
+        });
     }
 }
