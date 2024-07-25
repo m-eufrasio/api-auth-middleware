e96643f4dd74d95b319537015cc2fbd98b9366ed
Matheus Eufrasio <you@example.com>
2024-07-25 18:05:36 -0300

    feat: add gate middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,7 +58,7 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'gate' => \App\Http\Middleware\Gate::class,
+        'gate' => \App\Http\Middleware\GateAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
