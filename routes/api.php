c566e6b27e4fde61b804250e67edfb0cf82a1e92
Matheus Eufrasio <you@example.com>
2024-07-29 18:51:29 -0300

    docs: remove comments

--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,10 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::any('/home', [HomeController::class, 'home']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    // Route::get('/user', function (Request $request) {
-    //     return $request->user();
-    // });
-
     // Route::controller(PostController::class)->group(function () {
     //     Route::get('/index', 'index');
     // });
